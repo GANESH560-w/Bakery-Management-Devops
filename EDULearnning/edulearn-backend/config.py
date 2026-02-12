09ae0c92cc85e3c5d44f7410fe6eff17d37f8d58
Ganesh Jadhav <jadhavganesh1853@gmail.com>
2026-02-12 11:31:06 +0530

    Delete EDULearnning directory

--- EDULearnning/edulearn-backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    # Use PostgreSQL for RDS migration
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://dbadmin:SecurePassword123@localhost/edulearn'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your_super_secret_key'
